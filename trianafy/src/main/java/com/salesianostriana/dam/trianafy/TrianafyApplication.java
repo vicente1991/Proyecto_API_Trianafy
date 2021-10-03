98c66d0c3452c69d3afd981f15176014a66162ee
Mª Inmaculada Domínguez Vargas <onemayn@outlook.es>
2021-10-03 16:02:58 +0200

    Creación del proyecto

---
+++ trianafy/src/main/java/com/salesianostriana/dam/trianafy/TrianafyApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.trianafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrianafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrianafyApplication.class, args);
+	}
+
+}
