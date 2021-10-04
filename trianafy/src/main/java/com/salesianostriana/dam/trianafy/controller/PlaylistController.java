bbb3c45fbc27444acd15d0e3277f8523b84e8ca1
Mª Inmaculada Domínguez Vargas <onemayn@outlook.es>
2021-10-04 19:12:00 +0200

    Añadimos Playlist controller con la búsqueda de playlists

---
+++ trianafy/src/main/java/com/salesianostriana/dam/trianafy/controller/PlaylistController.java
@@ -0,0 +1,40 @@
+package com.salesianostriana.dam.trianafy.controller;
+
+import com.salesianostriana.dam.trianafy.dto.GetPlaylistDTO;
+import com.salesianostriana.dam.trianafy.dto.PlaylistDTOConverter;
+import com.salesianostriana.dam.trianafy.model.Playlist;
+import com.salesianostriana.dam.trianafy.repository.PlaylistRepository;
+import com.salesianostriana.dam.trianafy.repository.SongRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class PlaylistController {
+
+    private final PlaylistRepository repository;
+    private final PlaylistDTOConverter converter;
+    private final SongRepository songRepository;
+
+    @GetMapping("/lists")
+    public ResponseEntity<List<GetPlaylistDTO>> findAll(){
+
+        List<Playlist> lista = repository.findAll();
+
+        if(lista.isEmpty()){
+            return ResponseEntity.notFound().build();
+
+        }else {
+            List <GetPlaylistDTO> resultado = lista.stream()
+                                              .map(converter::playlistToGetPlaylistDTO)
+                                              .collect(Collectors.toList());
+
+            return ResponseEntity.ok().body(resultado);
+        }
+    }
+}
---
+++ trianafy/src/main/java/com/salesianostriana/dam/trianafy/dto/PlaylistDTOConverter.java
@@ -0,0 +1,17 @@
+package com.salesianostriana.dam.trianafy.dto;
+
+import com.salesianostriana.dam.trianafy.model.Playlist;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlaylistDTOConverter {
+
+    public GetPlaylistDTO playlistToGetPlaylistDTO(Playlist p) {
+        return GetPlaylistDTO
+                .builder().
+                nombre(p.getNombre())
+                .descripcion(p.getDescripcion())
+                .build();
+    }
+
+}
