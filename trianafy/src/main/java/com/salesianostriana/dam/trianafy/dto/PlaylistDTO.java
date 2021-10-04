a2b6e20cb1006fc94e6305488f09d336ae3434a3
Mª Inmaculada Domínguez Vargas <onemayn@outlook.es>
2021-10-04 16:47:52 +0200

    Creación entidad Playlist, con su repositorio y el DTO

---
+++ trianafy/src/main/java/com/salesianostriana/dam/trianafy/dto/PlaylistDTO.java
@@ -0,0 +1,17 @@
+package com.salesianostriana.dam.trianafy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PlaylistDTO {
+
+    private Long id;
+    private String nombre;
+    private String descripcion;
+}
