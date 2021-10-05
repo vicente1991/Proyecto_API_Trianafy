5bd2e6519fe4c56a3490e31d3ce4a9a3d93bd66c
Mª Inmaculada Domínguez Vargas <onemayn@outlook.es>
2021-10-05 16:17:22 +0200

    HU08 completada : Borrar una canción

--- trianafy/src/main/java/com/salesianostriana/dam/trianafy/controller/SongController.java
+++ trianafy/src/main/java/com/salesianostriana/dam/trianafy/controller/SongController.java
@@ -3,6 +3,7 @@ package com.salesianostriana.dam.trianafy.controller;
 import com.salesianostriana.dam.trianafy.model.Song;
 import com.salesianostriana.dam.trianafy.repository.SongRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,6 +36,13 @@ public class SongController {
                     return m;
                 })
         );
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
 
     }
 }
