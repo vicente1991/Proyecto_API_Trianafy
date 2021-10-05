874a61dbf3415fd48a9aea6acf115a93d6797b7b
ardana.mujua21 <cardamu02@gmail.com>
2021-10-05 13:47:48 +0200

    Añadida funcioalidad de modificar canciones

--- trianafy/src/main/java/com/salesianostriana/dam/trianafy/controller/SongController.java
+++ trianafy/src/main/java/com/salesianostriana/dam/trianafy/controller/SongController.java
@@ -5,20 +5,37 @@ import com.salesianostriana.dam.trianafy.repository.SongRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/songs")
 public class SongController {
 
     private final SongRepository repository;
 
-    @PostMapping("/songs")
+    @PostMapping("/")
     public ResponseEntity<Song> create(@RequestBody Song nueva){
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(nueva));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Song> edit(
+            @RequestBody Song s,
+            @PathVariable Long id) {
+
+        return ResponseEntity.of(
+                repository.findById(id).map(m -> {
+                    m.setTitulo(s.getTitulo());
+                    m.setArtista(s.getArtista());
+                    m.setAlbum(s.getAlbum());
+                    m.setAnio(s.getAnio());
+                    repository.save(m);
+                    return m;
+                })
+        );
+
     }
 }
 
